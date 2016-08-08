d0055ce97df861c033fb0a9d4fedd590209a6bc1
Jegan Lingam <jeganl@live.com>
2016-08-07 19:05:48 -0700

    add logging

--- DevWebApi/DevWebApi/Controllers/ProcessController.cs
+++ DevWebApi/DevWebApi/Controllers/ProcessController.cs
@@ -8,32 +8,44 @@ namespace DevWebApi
 {
 	public sealed class ProcessController : ApiController
 	{
-		// GET api/Process
-		public string Get(ProcessInfo processInfo)
-		{
-			PerformBasicAuthorization();
+		//// GET api/Process
+		//public string Get(ProcessInfo processInfo)
+		//{
+		//	PerformBasicAuthorization();
 
-			return "all good";
-		}
+		//	return "all good";
+		//}
 
 		// POST api/Process
 		public void Post([FromBody]ProcessInfo processInfo)
 		{
-			PerformBasicAuthorization();
-
-			var processStartInfo = new ProcessStartInfo(processInfo.FileName);
-			if (processInfo.StartAsAdministrator)
+			try
 			{
-				processStartInfo.Verb = "runas";
-			}
+				Trace.TraceInformation($"{DateTime.Now} - invoking process {processInfo.FileName}, {processInfo.StartAsAdministrator}");
 
-			Process.Start(processStartInfo);
+				PerformBasicAuthorization();
+
+				var processStartInfo = new ProcessStartInfo(processInfo.FileName);
+				if (processInfo.StartAsAdministrator)
+				{
+					processStartInfo.Verb = "runas";
+				}
+
+				Process.Start(processStartInfo);
+
+				Trace.TraceInformation($"{DateTime.Now} - process invoked successfully");
+			}
+			catch (Exception ex)
+			{
+				Trace.TraceError($"{DateTime.Now} - {ex.ToString()}");
+			}
 		}
 
 		private static void PerformBasicAuthorization()
 		{
 			if(!Thread.CurrentPrincipal.Identity.IsAuthenticated || WindowsIdentity.GetCurrent().Name != Thread.CurrentPrincipal.Identity.Name)
 			{
+				Trace.TraceWarning($"{DateTime.Now} - Authorization failed for {Thread.CurrentPrincipal.Identity.Name}");
 				new Exception($"{Thread.CurrentPrincipal.Identity.Name} caller not authorized!");
 			}
 		}
--- DevWebApi/DevWebApi/Program.cs
+++ DevWebApi/DevWebApi/Program.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.Owin.Hosting;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 
 namespace DevWebApi
 {
 	class Program
 	{
+		[STAThread]
 		static void Main(string[] args)
 		{
+			bool result;
+			var mutex = new System.Threading.Mutex(true, "DevWebApi", out result);
+			if (!result)
+			{
+				return;
+			}
+
 			HostWebApi();
 		}
 
@@ -18,14 +28,23 @@ namespace DevWebApi
 				try
 				{
 					string baseAddress = "http://localhost:9000/";
+					Trace.TraceInformation($"{DateTime.Now} - Starting up web api: {baseAddress}");
+
 					using (WebApp.Start<Startup>(url: baseAddress))
 					{
-						Console.ReadLine();
+						Trace.TraceInformation($"{DateTime.Now} - Started successfully");
+
+						do
+						{
+							Trace.TraceInformation($"{DateTime.Now} - Running");
+							Thread.Sleep(TimeSpan.FromMinutes(10));
+						}
+						while (true);
 					}
 				}
-				catch(Exception)
+				catch(Exception ex)
 				{
-					// Todo: Log exception
+					Trace.TraceError($"{DateTime.Now} - {ex.ToString()}");
 					Thread.Sleep(TimeSpan.FromMinutes(1));
 				}
 			}
